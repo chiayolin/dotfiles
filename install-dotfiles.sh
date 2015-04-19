fdd59bd7551a1679c8bdde8a815a675f9ab8f043
chiayolin <chiayo.lin@gmail.com>
2015-04-19 19:04:23 +0700

    added install script

---
+++ install-dotfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DOTFILESDIR=$(pwd -P)
+
+for DOTFILE in *; do
+	HOMEFILE="$HOME/.$DOTFILE"
+
+	# if dotfile is a directory
+	[ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
+	
+	# get full path
+	DIRFILE="$DOTFILESDIR/$DOTFILE"
+	
+	# skip non dotfiles
+	echo $DOTFILE | egrep -q '(dotfiles|\.txt|\.md)' \
+		&& continue
+	
+	# get *.sh files, remove it. then add a dot in front.
+	echo $DOTFILE | grep -q '\.sh' \
+		&& HOMEFILE="$HOME/.$(echo $DOTFILE | sed -e 's/\.sh//')"
+	echo $HOMEFILE
+	
+	# make symbolic links to ~/
+	if [ -L "$HOMEFILE" ] && ! [ -d $DOTFILE ]; then
+		ln -sfv "$DIRFILE" "$HOMEFILE"
+	else
+		rm -rv "$HOMEFILE"
+		ln -sv "$DIRFILE" "$HOMEFILE"
+	fi
+done
