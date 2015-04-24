79ae34b5887c913590c8779e38bb327210fb37fe
chiayolin <chiayo.lin@gmail.com>
2015-04-24 08:00:12 +0700

    tab -> two spaces

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,27 +2,27 @@
 DOTFILESDIR=$(pwd -P)
 
 for DOTFILE in *; do
-	HOMEFILE="$HOME/.$DOTFILE"
+  HOMEFILE="$HOME/.$DOTFILE"
 
-	# if dotfile is a directory
-	[ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
+  # if dotfile is a directory
+  [ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
 	
-	# get full path
-	DIRFILE="$DOTFILESDIR/$DOTFILE"
+  # get full path
+  DIRFILE="$DOTFILESDIR/$DOTFILE"
 	
-	# skip non dotfiles and old files with *.old
-	echo $DOTFILE | egrep -q '(dotfiles|\.txt|\.md|\.old)' \
-		&& continue
+  # skip non dotfiles and old files with *.old
+  echo $DOTFILE | egrep -q '(dotfiles|\.txt|\.md|\.old)' \
+    && continue
 	
-	# get *.sh files, remove it. then add a dot in front.
-	echo $DOTFILE | grep -q '\.sh' \
-		&& HOMEFILE="$HOME/.$(echo $DOTFILE | sed -e 's/\.sh//')"
+  # get *.sh files, remove it. then add a dot in front.
+  echo $DOTFILE | grep -q '\.sh' \
+    && HOMEFILE="$HOME/.$(echo $DOTFILE | sed -e 's/\.sh//')"
 	
-	# make symbolic links to ~/
-	if [ -L "$HOMEFILE" ] && ! [ -d $DOTFILE ]; then
-		ln -sfv "$DIRFILE" "$HOMEFILE"
-	else
-		[ -e "$HOMEFILE" ] && rm -rv "$HOMEFILE"
-		ln -sv "$DIRFILE" "$HOMEFILE"
-	fi
+  # make symbolic links to ~/
+  if [ -L "$HOMEFILE" ] && ! [ -d $DOTFILE ]; then
+    ln -sfv "$DIRFILE" "$HOMEFILE"
+  else
+    [ -e "$HOMEFILE" ] && rm -rv "$HOMEFILE"
+    ln -sv "$DIRFILE" "$HOMEFILE"
+  fi
 done
