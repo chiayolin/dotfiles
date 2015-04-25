cad589f9061059b9a8a6ee755867a828ef6f2d03
chiayolin <chiayo.lin@gmail.com>
2015-04-25 20:19:10 +0800

    tab to two spaces

--- shrc.sh
+++ shrc.sh
@@ -27,19 +27,19 @@ source ~/.aliases
 
 # platform-specific stuffs
 if [ $LINUX ]; then
-	# colorful ls
-	alias ls='ls --color=auto'
+  # colorful ls
+  alias ls='ls --color=auto'
 
-	# ibus input
-	export GTK_IM_MODULE=ibus
-	export XMODIFIERS=@im=ibus
-	export QT_IM_MODULE=ibus
+  # ibus input
+  export GTK_IM_MODULE=ibus
+  export XMODIFIERS=@im=ibus
+  export QT_IM_MODULE=ibus
 elif [ $OSX ]; then
-	export LC_ALL=en_US.UTF-8
+  export LC_ALL=en_US.UTF-8
   # brew update and upgrade
   [ "$(which brew)" ] && \
     alias buu='brew update && brew upgrade'
 elif [ $WINDOWS ]; then
-	# clear command for cygwin
-	alias clear='printf "\033c"'
+  # clear command for cygwin
+  alias clear='printf "\033c"'
 fi
