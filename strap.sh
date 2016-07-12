7109a835ba9a655f477ae6fa40164744ec76f1b7
chiayolin <chiayo.lin@gmail.com>
2016-07-12 10:39:55 -0400

    add installation confirmation and update errnos

--- strap.sh
+++ strap.sh
@@ -47,23 +47,39 @@ SUPPORTED_PKGS=$(find $PACKGE_DIR -type f -name "*.$OPSYS_NAME")
 # UISTERR - uninstallation error
 # UPDTERR - update error
 # RSRCERR - git remote show origin error
-# IPKGRRR - package installation error
+# IPKGERR - package installation error
 # UPKGERR - package uninstallation error
+# IOPTERR - invalid option
 # UKNOERR - unknown error(s)
 INSTERR=3; UISTERR=4; UPDTERR=5; 
-RSRCERR=6; IPKGRRR=7; UPKGERR=8;
-UKNOERR=9; SUCCESS=0;
+RSRCERR=6; IPKGERR=7; UPKGERR=8;
+IOPTERR=9; UKNOERR=10; SUCCESS=0; FAILURE=1
 
-# general function writting to stdout
+# _write_stdout - a general function writting to stdout
 _write_stdout () {
   echo "$PROGM_NAME: $1"
 }
 
-# general function writting to stderr
+# _write_stderr - a general function writting to stderr
 _write_stderr () {
   (2>&1 echo "$PROGM_NAME: error: $1")
 }
 
+_prompt_option () {
+  printf "$PROGM_NAME: do you want do continue (y/n)? "
+
+  local old_stty_config=$(stty -g)
+  stty raw -echo
+  local _option=$(head -c 1)
+  stty $old_stty_config # restore old stty config
+  echo ""
+  
+  $(echo "$_option" | grep -iq "^y") && return 0
+  _write_stdout "package installation canceled, aborting..."
+  return 1
+}
+
+# install_dotfiles - install the dotfiles within $DOTFLE_DIR
 install_dotfiles () {
   cd ./$DOTFLE_DIR
   for dotfile in *; do
@@ -93,24 +109,33 @@ install_dotfiles () {
   cd $ORIGIN_DIR
 }
 
+# install_dotfiles - uninstall the dotfiles under $HOME
 uninstall_dotfiles () {
   # remove the symlinks to $ORIGIN_DIR at $HOME
   rm -fv $(find $HOME -type l -ls -maxdepth 1 | \
     grep "$ORIGIN_DIR" | awk '{ print $11 }' | xargs)
 }
 
+# install_packages - install $SUPPORTED_PKGS by following the rules 
+#                    defined under $PACKGE_DIR
 install_packages () {
+  _write_stdout "the following packages will be installed:"
+  for package in $SUPPORTED_PKGS; do echo "  $package"; done
+  _prompt_option || exit $SUCCESS
+
   for package in $SUPPORTED_PKGS; do
-    _write_stdout "installing $ORIGIN_DIR/$package..."   
+    _write_stdout "fetching $ORIGIN_DIR/$package..."   
     source $(echo $package | sed -e "s/\.$OPSYS_NAME//").info && \
       source $package install
   done
 }
 
+# uninstall_packages - uninstall the packages installed
 uninstall_packages () {
   _write_stdout "please uninstall the installed packages manually."
 }
 
+# fetch_update - fetch updates from remote source
 fetch_update () {
   # fetch the latest update
   [ -d "$ORIGIN_DIR/.git" ] && \
@@ -119,19 +144,25 @@ fetch_update () {
         git submodule update
 }
 
+# _error_handling - handling error messages
 _error_handling () {
   local prefix="failed to"
   local suffix="dotfiles"
+  local suffix_pkg="packages"
   case $1 in
-    3) _write_stderr "$prefix install $suffix"
+    $INSTERR) _write_stderr "$prefix install $suffix"
       ;;
-    4) _write_stderr "$prefix uninstall $suffix"
+    $UISTERR) _write_stderr "$prefix uninstall $suffix"
       ;;
-    5) _write_stderr "$prefix update $suffix"
+    $UPDTERR) _write_stderr "$prefix update $suffix"
       ;;
-    6) _write_stderr "$prefix determine repo's remote source"
+    $RSRCERR) _write_stderr "$prefix determine repo's remote source"
       ;;
-    7) _write_stderr "invalid option: $ARG"
+    $IPKGERR) _write_stderr "$prefix install $suffix_pkg"
+      ;;
+    $UPKGERR) _write_stderr "$prefix uninstall $suffix_pkg"
+      ;;
+    $IOPTERR) _write_stderr "invalid option: $ARG"
        echo && print_help
       ;;
     # put the installing error thingy here too
@@ -139,9 +170,10 @@ _error_handling () {
       ;;
   esac
 
-  exit 1
+  exit $FAILURE
 }
 
+# print_help - print help
 print_help () {
   echo "usage: $PROGM_NAME [option]"
   echo "options:"
@@ -153,33 +185,34 @@ print_help () {
   echo "  uninstall-pkg  uninstall packages from the machine"
 }
 
+# main - the good old main function
 main () {
   if [ -z $1 ]; then
     print_help && exit $SUCCESS
   elif [ $1 == "install" ]; then
     _write_stdout "installing dotfiles..."
-    install_dotfiles || return 3
+    install_dotfiles || return $INSTERR
   
   elif [ $1 == "uninstall" ]; then
     _write_stdout "uninstalling dotfiles..."
-    uninstall_dotfiles || return 4
+    uninstall_dotfiles || return $UISTERR
   
   elif [ $1 == "update" ]; then
     _write_stdout "fetching updates..."
-    fetch_update || return 5
+    fetch_update || return $UPDTERR
 
   elif [ $1 == "show" ]; then
     _write_stdout "determining repo's remote origin..."
-    git remote show origin || return 6
+    git remote show origin || return $RSRCERR
   
   elif [ $1 == "install-pkg" ]; then
-    install_packages || return 7
+    install_packages || return $IPKGERR
   
   elif [ $1 == "uninstall-pkg" ]; then
-    uninstall_packages || return 8
+    uninstall_packages || return $UPKGERR
   
   else
-    return 9
+    return $UKNOERR
   fi
 }
 
